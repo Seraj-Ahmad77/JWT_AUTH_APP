e9db7d765881d74f74257b926c8e88c15e393e44
Seraj Ahmad <serajansari7704@gmail.com>
2025-11-03 10:52:52 +0530

    new commit

--- Backend/controller/userController.js
+++ Backend/controller/userController.js
@@ -2,79 +2,73 @@ import ErrorHandler from "../middleware/errorMiddleware.js";
 import { catchAsyncErrors } from "../middleware/catchAsyncError.js";
 import { User } from "../models/userSchema.js";
 import { sendEmail } from "../utils/sendEmail.js";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import twilio from "twilio";
 import { sendToken } from "../utils/sendToken.js";
-import crypto from "crypto"
-dotenv.config({path:"./config/config.env"})
+import crypto from "crypto";
+dotenv.config({ path: "./config/config.env" });
 const client = twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
 
 export const register = catchAsyncErrors(async (req, res, next) => {
-  
-    const { name, email, phone,password, verificationMethod } = req.body;
+  const { name, email, phone, password, verificationMethod } = req.body;
 
-    // Check if all fields are provided
-    if (!name || !email || !password || !phone || !verificationMethod) {
-      return next(new ErrorHandler("Required all fields.", 400));
-    }
+  // Check if all fields are provided
+  if (!name || !email || !password || !phone || !verificationMethod) {
+    return next(new ErrorHandler("Required all fields.", 400));
+  }
 
-    // Function to validate phone number format (India format)
-    function validatePhoneNumber(phone) {
-      const phoneRegex = /^\+91?\d{10}$/;
-      return phoneRegex.test(phone);
-    }
+  // Function to validate phone number format (India format)
+  function validatePhoneNumber(phone) {
+    const phoneRegex = /^\+91?\d{10}$/;
+    return phoneRegex.test(phone);
+  }
 
-    // Validate phone number
-    if (!validatePhoneNumber(phone)) {
-      return next(new ErrorHandler("Invalid phone number.", 400));
-    }
+  // Validate phone number
+  if (!validatePhoneNumber(phone)) {
+    return next(new ErrorHandler("Invalid phone number.", 400));
+  }
 
-    // Check if the user already exists with the given email or phone
-    const existUser = await User.findOne({
-      $or: [
-        { email, accountVerified: true },
-        { phone, accountVerified: true },
-      ],
-    });
+  // Check if the user already exists with the given email or phone
+  const existUser = await User.findOne({
+    $or: [
+      { email, accountVerified: true },
+      { phone, accountVerified: true },
+    ],
+  });
 
-    if (existUser) {
-      return next(
-        new ErrorHandler("Phone or email is already registered", 400)
-      );
-    }
+  if (existUser) {
+    return next(new ErrorHandler("Phone or email is already registered", 400));
+  }
 
-    // Check if there are more than 3 unsuccessful registration attempts for this email or phone
-    const attempByUser = await User.find({
-      $or: [
-        { email, accountVerified: false },
-        { phone, accountVerified: false },
-      ],
-    });
-    if (attempByUser.length > 3) {
-      return next(
-        new ErrorHandler(
-          "You have exceeded the maximum number of attempts.",
-          400
-        )
-      );
-    }
-
-    // Create user and generate the verification code
-    const userdata = { name, email, password, phone };
-    const user = await User.create(userdata);
-    const verificationCode = await user.generateVerificationCode();
-    await user.save();
-
-    // Send verification code based on the method (email or phone)
-    sendVerificationCode(
-      verificationMethod,
-      verificationCode,
-      name,
-      email,
-      phone,
-      res,
-      next
+  // Check if there are more than 3 unsuccessful registration attempts for this email or phone
+  const attempByUser = await User.find({
+    $or: [
+      { email, accountVerified: false },
+      { phone, accountVerified: false },
+    ],
+  });
+  if (attempByUser.length > 3) {
+    return next(
+      new ErrorHandler("You have exceeded the maximum number of attempts.", 400)
     );
+  }
+
+  // Create user and generate the verification code
+  const userdata = { name, email, password, phone };
+  const user = await User.create(userdata);
+  const verificationCode = await user.generateVerificationCode();
+  await user.save();
+
+  // Send verification code based on the method (email or phone)
+  sendVerificationCode(
+    verificationMethod,
+    verificationCode,
+    name,
+    email,
+    phone,
+    res,
+    next
+  );
 });
 
 async function sendVerificationCode(
@@ -123,7 +117,7 @@ async function sendVerificationCode(
       });
     }
   } catch (error) {
-    console.log("error ",error);
+    console.log("error ", error);
     return next(new ErrorHandler("Verification code failed to send.", 500)); // Pass the error to the error handling middleware
   }
 }
@@ -165,7 +159,7 @@ export const verifyOtp = catchAsyncErrors(async (req, res, next) => {
 
     if (existingUser) {
       return next(new ErrorHandler("User already exists.", 200));
-    } 
+    }
 
     const userAllEnties = await User.find({
       $or: [
@@ -199,7 +193,7 @@ export const verifyOtp = catchAsyncErrors(async (req, res, next) => {
     const verificationCodeExpire = new Date(
       user.verificationCodeExpire
     ).getTime();
-  
+
     if (currentDate > verificationCodeExpire) {
       return next(new ErrorHandler("otp expired", 400));
     }
@@ -215,94 +209,117 @@ export const verifyOtp = catchAsyncErrors(async (req, res, next) => {
   }
 });
 
-export const login=catchAsyncErrors(async(req,res,next)=>{
-  const {email,password}=req.body;
+export const login = catchAsyncErrors(async (req, res, next) => {
+  const { email, password } = req.body;
 
-  if(!email || !password){
-    return next(new ErrorHandler("Email and password are required.",400));
+  if (!email || !password) {
+    return next(new ErrorHandler("Email and password are required.", 400));
   }
 
-  const user=await User.findOne({email,accountVerified:true}).select("+password");
-  if(!user){
-    return next(new ErrorHandler("Invalid email or phone.",400));
+  const user = await User.findOne({ email, accountVerified: true }).select(
+    "+password"
+  );
+  if (!user) {
+    return next(new ErrorHandler("Invalid email or phone.", 400));
   }
-  const isPasswordMatch=await user.comparePassword(password);
-  if(!isPasswordMatch){
-    return next(new ErrorHandler("Invalid email or password.",400));
+  const isPasswordMatch = await user.comparePassword(password);
+  if (!isPasswordMatch) {
+    return next(new ErrorHandler("Invalid email or password.", 400));
   }
-  sendToken(user,200,"User logged in successfully.",res)
-})
+  sendToken(user, 200, "User logged in successfully.", res);
+});
 
-export const logout=catchAsyncErrors(async(req,res,next)=>{
-  res.status(200).cookie("token","",{
-    expires:new Date(Date.now()),
-    httpOnly:true,
-  }).json({
-    success:true,
-    message:"User logged out successfully."
-  })
-})
+export const logout = catchAsyncErrors(async (req, res, next) => {
+  res
+    .status(200)
+    .cookie("token", "", {
+      expires: new Date(Date.now()),
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+    })
+    .json({
+      success: true,
+      message: "User logged out successfully.",
+    });
+});
 
-
-export const getUser=catchAsyncErrors(async(req,res,next)=>{
-  const user=req.user;
+export const getUser = catchAsyncErrors(async (req, res, next) => {
+  const user = req.user;
   res.status(200).json({
-    success:true,
+    success: true,
     user,
-  })
-})
+  });
+});
 
-export const forgotPassword=catchAsyncErrors(async(req,res,next)=>{
-  const user=await User.findOne({email:req.body.email,accountVerified:true});
-  if(!user)
-  {
-    return next(new ErrorHandler("User not found",404));
+export const forgotPassword = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findOne({
+    email: req.body.email,
+    accountVerified: true,
+  });
+  if (!user) {
+    return next(new ErrorHandler("User not found", 404));
   }
 
-  const resetToken=user.generateResetPasswordToken();
-  await user.save({validateBeforeSave:false});
-  const resetPaaawosdUrl=`${process.env.FRONTEND_URL}/password/reset/${resetToken}`;
+  const resetToken = user.generateResetPasswordToken();
+  await user.save({ validateBeforeSave: false });
+  const resetPaaawosdUrl = `${process.env.FRONTEND_URL}/password/reset/${resetToken}`;
 
-  const message=` Your reset password token is :--\n\n ${resetPaaawosdUrl} \n\n if you have not requested this email please ignore it.`
+  const message = ` Your reset password token is :--\n\n ${resetPaaawosdUrl} \n\n if you have not requested this email please ignore it.`;
 
   try {
-    sendEmail({email:user.email,subject:"Authentication reset Password",message,})
+    sendEmail({
+      email: user.email,
+      subject: "Authentication reset Password",
+      message,
+    });
     res.status(200).json({
-      success:true,
-      message:` Email sent to ${user.email} successfully.`
-    })
-    
+      success: true,
+      message: ` Email sent to ${user.email} successfully.`,
+    });
   } catch (error) {
-    user.resetPasswordToken=undefined;
-    user.resetPasswordExpire=undefined;
-    await user.save({validateBeforeSave:false})
-    return next(new ErrorHandler(error.message?error.message:"Cannot send reset password token",500));
-    
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+    await user.save({ validateBeforeSave: false });
+    return next(
+      new ErrorHandler(
+        error.message ? error.message : "Cannot send reset password token",
+        500
+      )
+    );
+  }
+});
+
+export const resetPassword = catchAsyncErrors(async (req, res, next) => {
+  const { token } = req.params;
+  const resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(token)
+    .digest("hex");
+  const user = await User.findOne({
+    resetPasswordToken,
+    resetPasswordExpire: { $gt: Date.now() },
+  });
+
+  if (!user) {
+    return next(
+      new ErrorHandler(
+        "Reset password token is invalid or has been expired.",
+        400
+      )
+    );
   }
 
-})
-
-export const resetPassword=catchAsyncErrors(async(req,res,next)=>{
-  const {token}=req.params;
-  const resetPasswordToken=crypto.createHash("sha256").update(token).digest("hex");
-  const user=await User.findOne({resetPasswordToken,
-    resetPasswordExpire:{$gt:Date.now()}
-  })
-
-  if(!user)
-  {
-    return next(new ErrorHandler("Reset password token is invalid or has been expired.",400))
+  if (req.body.password !== req.body.confirmPassword) {
+    return next(
+      new ErrorHandler("password and confirm password do not match.", 400)
+    );
   }
-
-  if(req.body.password!==req.body.confirmPassword){
-    return next(new ErrorHandler("password and confirm password do not match.",400))
-  }
-  user.password=req.body.password;
-  user.resetPasswordToken=undefined;
-  user.resetPasswordExpire=undefined;
+  user.password = req.body.password;
+  user.resetPasswordToken = undefined;
+  user.resetPasswordExpire = undefined;
 
   await user.save();
 
-  sendToken(user,200,"Reset password successfully.",res);
-})
-
+  sendToken(user, 200, "Reset password successfully.", res);
+});
