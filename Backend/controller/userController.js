f97f2a13a2ee804d6d40d8e8b338804392a76647
Seraj Ahmad <serajansari7704@gmail.com>
2025-11-03 10:35:27 +0530

    new update

--- Backend/controller/userController.js
+++ Backend/controller/userController.js
@@ -123,6 +123,7 @@ async function sendVerificationCode(
       });
     }
   } catch (error) {
+    console.log(error);
     return next(new ErrorHandler("Verification code failed to send.", 500)); // Pass the error to the error handling middleware
   }
 }
